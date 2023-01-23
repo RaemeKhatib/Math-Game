f68834ddb03df7c8eb381c70ed1198619f92f557
Raeme Khatib <raeme.khatib@telus.com>
2023-01-23 13:28:01 -0700

    add logic if one question is asked

--- main.rb
+++ main.rb
@@ -9,3 +9,30 @@ player = 1
 count = 0
 
 # Start off the game with Both players being alive and 1st player turn
+until player1.lives == 0 && player2.lives == 0
+  if player == 1
+    if count !=0 
+      puts '---------- NEW TURN ----------'
+    end
+    question = Question.new
+    puts "Player 1: #{question}"
+    print "Enter Answer>"
+    answer = gets.chomp.to_i
+  if answer == question.answer
+    puts 'Player 1: YES! You are correct.'
+      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      player = 2
+      count +=1
+    else
+      player1.lives -=1
+      puts 'Player 1: Seriously? No!'
+      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      if player1.lives == 0
+        break
+      end
+      player = 2
+      count +=1
+    end
+
+  end
+end
--- question.rb
+++ question.rb
@@ -3,10 +3,10 @@ class Question
   attr_accessor :question, :answer
 
   def question(player)
-    @number1 = rand(1...20)
-    @number2 = rand(1...20)
-    @question = "What does #{@number1} plus #{@number2} equal?"
-    @answer = number1 + number2
+    @number1 = rand(1..20)
+    @number2 = rand(1..20)
+    @question = "What is #{@number1} plus #{@number2} equal?"
+    @answer = @number1 + @number2
   end
 
 end
