69198856817c8695e8115f5705bb89dc393e7ff7
Raeme Khatib <raeme.khatib@telus.com>
2023-01-23 10:55:33 -0700

    add a class for math questions

---
+++ question.rb
@@ -0,0 +1,10 @@
+class Question 
+
+  def initialize(number1, number2)
+    @number1 = number1
+    @number2 = number2
+    @question = "What does #{@number1} plus #{@number2} equal?"
+    @answer = @number1 + @number2
+  end
+
+end
